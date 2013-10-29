df553f376919c6b315b8da3244716dd4053b56ea
osianmoreno <morenosoftware@gmail.com>
2013-10-29 18:24:11 +0100

    Hola soy Brian !

--- ProjectoComun/src/ProjectoComun.java
+++ ProjectoComun/src/ProjectoComun.java
@@ -1,7 +1,5 @@
 public class ProjectoComun {
 	public static void main ( String [] args) {
-		System.out.print("holacarapapa");
-		System.out.print("holacarapapa");
-		System.out.print("holacarapapa");
+		System.out.print("Hola soy Brian");
 	}
 }
