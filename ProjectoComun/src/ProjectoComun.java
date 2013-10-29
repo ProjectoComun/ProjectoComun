7560e3abb48f8219a267c70223a88d38dcee85a9
osianmoreno <morenosoftware@gmail.com>
2013-10-29 18:15:54 +0100

    MI PRIMER COMMIT !

--- ProjectoComun/src/ProjectoComun.java
+++ ProjectoComun/src/ProjectoComun.java
@@ -2,5 +2,6 @@ public class ProjectoComun {
 	public static void main ( String [] args) {
 		System.out.print("holacarapapa");
 		System.out.print("holacarapapa");
+		System.out.print("holacarapapa");
 	}
 }
